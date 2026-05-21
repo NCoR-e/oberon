58238496c6aa4df43042482018e69a9206d0ac4f
Dmitriy <codingncrploy@gmail.com>
2026-05-22 02:39:42 +0300

    chore: Adding linters, with settings: the basic minimum, as well as settings to prevent conflicts between them.

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier'
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs}"],
+
+    plugins: { 
+      js, 
+      prettier: prettierPlugin
+    },
+
+    extends: ["js/recommended", prettierConfig],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      //...
+    }
+  },
+]);
